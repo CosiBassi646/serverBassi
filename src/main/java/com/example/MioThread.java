e2e0d5e39dc6ed150e03fb52d2bf157b2ac609d6
lai2-24 <lai2-24@itismeucci.com>
2024-10-22 14:16:18 +0200

    fine seconda ora

--- src/main/java/com/example/MioThread.java
+++ src/main/java/com/example/MioThread.java
@@ -8,6 +8,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.util.Random;
+import java.lang.NumberFormatException;
 
 public class MioThread extends Thread {
 
@@ -15,7 +16,7 @@ public class MioThread extends Thread {
     BufferedReader in;//input
     DataOutputStream out;
     int numero;
-    int tentativi = 0;
+    int tentativi = 1;
 
     public MioThread(Socket s, int numero) throws IOException{
         this.s = s;
@@ -25,52 +26,79 @@ public class MioThread extends Thread {
     }
     @Override
     public void run() {
-        int intInserito;
-        String numeroInserito = null;
+        int intInserito = 0;
+        String numeroInserito = "";
         System.out.println(numero);
         do{
-            
+
             try {
                 numeroInserito = in.readLine();
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            intInserito = Integer.parseInt(numeroInserito);
-
-            //controllo del numero inserito
-            if(intInserito == numero){ 
-                //se è uguale:
+           
+           //se il client invia un vaore null
+            if(numeroInserito.equals("")){
                 try {
-                    out.writeBytes("=" + "\n");
-                    break;
+                    out.writeBytes("errore valore null, riprova l'inserimento" + "\n");
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
             }else{
-                if (intInserito > numero) {
-                    //se è minore: 
+                intInserito = Integer.parseInt(numeroInserito);
+
+                //se il numero è fuori dal range 0-100
+                if(intInserito > 100){
                     try {
-                        out.writeBytes("<" + "\n");
+                        out.writeBytes("Il numero inserito non rientra nell'intervallo previsto dal software " + "\n");
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    //non conto gli errori come tentativi
+                }else{
+                   
+                //controllo del numero inserito
+                if(intInserito == numero){ 
+                    //se è uguale:
+                    try {
+                        out.writeBytes("=" + "\n");
+                        break;
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
                 }else{
-                    //se è maggiore:
-                    if (intInserito < numero) {
+                    if (intInserito > numero) {
+                        //se è minore: 
                         try {
-                            out.writeBytes(">" + "\n");
+                            out.writeBytes("<" + "\n");
                         } catch (IOException e) {
                             e.printStackTrace();
                         }
+                    }else{
+                        //se è maggiore:
+                        if (intInserito < numero) {
+                            try {
+                                out.writeBytes(">" + "\n");
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
                     }
                 }
+                tentativi++;
+                }
             }
-            tentativi++;
+                
+        
+
+
+          
+
         }while(intInserito != numero);
 
         //invio il numeor di tentativi impiegati
         try {
-            out.writeBytes(tentativi + "\n");
+            out.writeBytes( "" + tentativi + "\n");
         } catch (IOException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
